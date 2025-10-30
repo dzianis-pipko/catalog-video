e6f63dde6872f989398ba5515d85192eb399b0e7
Denis <dzianis.pipko@gmail.com>
2025-10-30 03:09:23 +0300

    feat: create a video card component with a preview and implement a grid

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ const Layout = ({ children }: PropsWithChildren<unknown>) => {
 
 	return (
 		<main
-			className={'flex min-h-screen'}
+			className={'flex min-h-screen justify-center'}
 		>
 			<section className='p-layout'>{children}</section>
 		</main>
